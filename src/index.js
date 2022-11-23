4ccbee8145914f246b66bfad68375c6dc988b573
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-11-23 17:39:02 +0200

    added spa feature to page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import './style.css';
 import Get from './modules/get.js';
 import Counters from './modules/counters.js';
+import Showsection from './modules/nav.js';
+
+document.getElementById('home').addEventListener('click', () => {
+  Showsection.showList(); // switch to list section.
+});
+document.getElementById('contact').addEventListener('click', () => {
+  Showsection.showcontact(); // switch to contact section.
+});
+document.getElementById('about').addEventListener('click', () => {
+  Showsection.showabout(); // switch about section.
+});
 
 // making sure the widow is loaded then all data is provided.
 window.onload = async () => {
